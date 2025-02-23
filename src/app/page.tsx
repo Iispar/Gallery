8d866524e7744938f6d425955610b7699c8d778f
Iiro Partanen <iiro.s.partanen@gmail.com>
2025-02-23 19:19:23 +0200

    home to /

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,44 @@
+"use client";
+
+import { Canvas } from "@react-three/fiber";
+import View from "./home/View";
+import { Suspense } from "react";
+import { Html } from "@react-three/drei";
+
 export default function Home() {
-  return <div>home</div>;
+  return (
+    <Canvas
+      shadows
+      style={{
+        height: "100vh",
+      }}
+    >
+      <directionalLight
+        position={[0, 5, 4]}
+        intensity={4}
+        color={0xffffff}
+        shadow-camera-left={-30}
+        shadow-camera-right={30}
+        castShadow
+      />
+      <Suspense
+        fallback={
+          <>
+            <Html>
+              <div
+                style={{
+                  position: "absolute",
+                  left: "-30px",
+                }}
+              >
+                loading...
+              </div>
+            </Html>
+          </>
+        }
+      >
+        <View />
+      </Suspense>
+    </Canvas>
+  );
 }
