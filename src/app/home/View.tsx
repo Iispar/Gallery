06491f7816731afde29fd397c6a2ac9479b237e0
Iiro Partanen <iiro.s.partanen@gmail.com>
2025-02-27 19:01:14 +0200

    damping based on device

--- src/app/home/View.tsx
+++ src/app/home/View.tsx
@@ -3,7 +3,7 @@
 
 import { useThree } from "@react-three/fiber";
 import { Scroll, ScrollControls } from "@react-three/drei";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { painting } from "../data/paintings";
 
 import PaintingsRoom from "./PaintingsRoom";
@@ -11,18 +11,38 @@ import PaintingsRoom from "./PaintingsRoom";
 export default function View() {
   const { width } = useThree((state: any) => state.viewport);
   const [clicked, setClicked] = useState(null);
+  const [damping, setDamping] = useState(0);
 
   const w = 2.2;
-
   const gap = 3;
-
   const xW = w + gap;
+
+  const handleWheel = (event: WheelEvent) => {
+    if (event.deltaY !== 0) {
+      setDamping(0.2);
+    }
+  };
+
+  // Event handler for touch scroll
+  const handleTouchStart = () => {
+    setDamping(0);
+  };
+
+  useEffect(() => {
+    window.addEventListener("wheel", handleWheel);
+    window.addEventListener("touchstart", handleTouchStart);
+
+    return () => {
+      window.removeEventListener("wheel", handleWheel);
+      window.removeEventListener("touchstart", handleTouchStart);
+    };
+  }, []);
 
   return (
     <ScrollControls
       horizontal
       distance={0.9}
-      damping={0}
+      damping={damping}
       pages={(width / 3 - xW + (painting.length + 1) * xW) / width}
       enabled={clicked === null}
       prepend
